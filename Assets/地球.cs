26c952c20a08c330256fe79a98072a35c16f12ac
張_硯_筑 <alice000528@gmail.com>
2018-10-09 00:04:08 +0800

    Initial commit

---
+++ Assets/地球.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 地球 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, 1.5f, 0);
+        transform.Rotate(angle);
+
+        
+        
+    }
+}
